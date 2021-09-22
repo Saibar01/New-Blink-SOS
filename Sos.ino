99649099d47686b7b41d81777548f26012f2b162
Saibar01 <91179474+Saibar01@users.noreply.github.com>
2021-09-22 09:24:39 +0200

    Create Sos.ino

---
+++ Sos.ino
@@ -0,0 +1,82 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);  
+  digitalWrite(LED_BUILTIN, HIGH);     
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);
+  digitalWrite(LED_BUILTIN, HIGH);     
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);
+   digitalWrite(LED_BUILTIN, HIGH);     
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);
+  digitalWrite(LED_BUILTIN, HIGH);     
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);
+   digitalWrite(LED_BUILTIN, HIGH);     
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);  
+  digitalWrite(LED_BUILTIN, HIGH);     
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(250);
+  digitalWrite(LED_BUILTIN, HIGH);     
+  Serial.print("LED-a  ");           
+  Serial.println("ON");
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);    
+  Serial.print("LED-a  ");           
+  Serial.println("OFF");
+  delay(2000);
+}
